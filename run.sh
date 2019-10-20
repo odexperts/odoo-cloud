dfcaee938b74af5a33eafe668ef33a26d52eb56b
Russell Briggs <russ@paraflyer.net>
2019-10-20 16:04:56 +1300

    Make sure we run the correct tag

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 set -x
 
 # base image
-docker run --rm -it -p 8069:8069 --env-file=./odoo.env odoo-cloud $@
+docker run --rm -it -p 8069:8069 --env-file=./odoo.env odoo-cloud:12 $@
 
 # with volume mounts
-# docker run --rm -it -v $PWD/cloud_addons:/mnt/odoo/cloud_addons -v $PWD/odoo_data:/mnt/odoo/data -p 8069:8069 --env-file=./odoo.env odoo-cloud $@
+# docker run --rm -it -v $PWD/cloud_addons:/mnt/odoo/cloud_addons -v $PWD/odoo_data:/mnt/odoo/data -p 8069:8069 --env-file=./odoo.env odoo-cloud:12 $@
